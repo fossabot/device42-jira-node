d79d05274228578fd159baef538c4692984a4fc9
Kreig Zimmerman <kreig303@yahoo.com>
2017-08-18 13:04:24 -0700

    Up to actual server invocation.  Awaiting final pieces (manipulate data, create JIRA ticket)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # d42test
+
+To invoke: `git clone` to local, `npm install` and then `node server.js` to run.
---
+++ server.js
@@ -0,0 +1,76 @@
+'use strict';
+// add required libs
+const Hapi = require('hapi'); // the router sw
+const Path = require('path'); // allows relative paths
+const Joi = require('joi'); // allows parameter checks
+const Hoek = require('hoek');
+
+// invoke server object
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+});
+server.connection({ port: 3000 });
+// default action on `/`
+server.route({
+    method: ['POST', 'PUT'],
+    path: '/',
+    handler: function (request, reply) {
+        reply('created').code(201); // do-something
+    },
+});
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello!'); // do-something
+    },
+});
+
+// adding logging
+const Good = require('good');
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+    // launch server process and wait
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
+
+server.register(require('vision'), (err) => {
+    Hoek.assert(!err, err);
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: 'templates'
+    });
+});
